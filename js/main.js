61d44380cba5ea01f34fe0eeaf0340eea8093248
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-02-23 10:24:38 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/*----- constants -----*/
+// audio for boat taking a shot (BONUS)
+// audio for computer hitting a boat (BONUS)
+// different boat sizes
+// hit/miss/open square colors
+
+
+/*----- app's state (variables) -----*/
+// winner
+// turn
+// player's boat positions / board
+// computer's boat positions / board
+// player's guess shots / board
+// computer's guess shots / board
+
+
+/*----- cached element references -----*/
+// button for take shot
+// button for new game
+// button for boat positions
+// button for starting a game
+// player's boat board
+// player's guess board
+// display to log messages of in-game moments
+// each square element square
+// each board element square
+
+
+/*----- event listeners -----*/
+// listen for button click when the player wants to take a shot
+// listen for button click when a new game wants to be started (reset)
+// listen for button click when player chooses to set boat positions
+// listen for button click when player chooses to start a game
+// listen for square clicked
+
+
+/*----- functions -----*/
+// handleGuess()     // checks for hit or miss
+// getWinner()       // determines if all boats were hit
+// getRandomGuess()  // gets computer guess
+// placeBoat()       // places a boat when creating fleet layout
+// init()            // initializes the game
+// render()          // renders the game UI
+// renderGuess()     // changes the UI based on the guess
+// renderGameLog()   // changes the log of game events
+// renderControls()  // shows/hides buttons
+// getRandomBoats()  // creates computer fleet layout (not necessary)  CAN MAKE THIS A TWO PLAYER GAME
+// startGame()       // starts a game after the player creates their fleet layout 
+// resetGame()       // resets the game board and scores
+
+
+// To add sound -> tutorial to add sound on the TI
